bd460170feabb9ea305ff7394f53f1d8cef77ba8
Mayank2k <53013993+manticore20@users.noreply.github.com>
2021-03-06 18:13:39 +0530

    -

---
+++ Async Js/CallbackFunctioninHttpReq.js
@@ -0,0 +1,20 @@
+const getTodos=()=>{
+const request=new XMLHttpRequest();
+//console.log(request); this give us the request object
+
+request.addEventListener('readystatechange',()=>{
+    if(request.readyState === 4 && request.status===200){
+        console.log(request,request.responseText);
+    }
+    else if(request.readyState === 4){
+        console.log('no data fetched');
+    }
+});
+
+request.open('GET','https://jsonplaceholder.typicode.com/todos/');
+request.send();
+
+};
+
+
+getTodos();
